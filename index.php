892d6c425c96a4e29219113dcf6f31b596ac5a0c
Rajendra arora <rajendraarora16@yahoo.com>
2017-11-14 19:50:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
-<?php 
-    echo "Hello world";
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$url = 'https://api-ssl.bitly.com/v3/shorten?access_token=b0023184f8bca0f0f5f302fe71909f27de077ab6&longUrl='.rawurlencode($_GET['longurl']);
+
+// Display errors
+ini_set('display_errors',1);
+header('Content-Type:application/json; charset=utf-8');
+// Set up options
+$toSet[CURLOPT_CONNECTTIMEOUT] = 20; // Time to wait for connection
+$toSet[CURLOPT_TIMEOUT] = 45; // Time to wait for whole operation
+$toSet[CURLOPT_RETURNTRANSFER] = true; // Return transfer instead of printing
+$toSet[CURLOPT_FAILONERROR] = true;
+
+// Forward the custom headers
+if ( isset($_SERVER['HTTP_USER_AGENT']) ) $toSet[CURLOPT_USERAGENT] = $_SERVER['HTTP_USER_AGENT'];
+
+// Show SSL without verifying
+$toSet[CURLOPT_SSL_VERIFYPEER] = false;
+$toSet[CURLOPT_SSL_VERIFYHOST] = false;
+
+// Start curl
+$curl = curl_init($url);
+
+// Load options
+curl_setopt_array($curl, $toSet);
+
+// Fetch page
+$return = curl_exec($curl);
+
+// Check for curl errors
+if ( $error = curl_error($curl) ) 
+ echo 'ERROR: ',$error;
+
+// Close handle
+curl_close($curl);
+
+echo str_replace('\/','/',$return);
+
+
 ?>
